422ba6994ede6db55a7990ac2e336d58aaa59dd3
Esin Shadrach Asukwo <esinshadrachrafe@gmail.com>
2022-07-24 23:31:10 +0100

    Update main.py
    an upgrade to the original project

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-import requests
-import os
-# manga_name = input("enter folder name>>> ")
-manga_name = ""
+import requests as re
+import pyttsx3 as tts
+# https://scans-hot.leanbox.us/manga/Boku-No-Hero-Academia/0359-001.png use this one for mangas at times
+# https://scans-hot.leanbox.us/manga/Onepunch-Man/0166-001.png
+# https://temp.compsci88.com/manga/Boku-No-Hero-Academia/0360-001.png
+# https://official-other.orience.us/manga/The-God-Of-High-School/0112-004.png
+name = input("enter name>>")
+postname = name.replace(" ", "-")
+manganame = postname.title()
+chapter_num = int(input("enter chapter number>>"))
+lim = 60
 
-page1 = "https://temp.compsci88.com/manga/Onepunch-Man/0165-001.png"
-page_file_name1 = "1-2.png"
+def manga_downloader_edited():
+    page_number = 0
+    page_name = 0
+    while page_name < lim:
+        page_number += 1
+        page_name += 1
+        with open(f"{manganame}_{chapter_num}_{page_name}.png", "wb") as f:
+            if page_number < 10:
+                image = re.get(
+                    f"https://official-other.orience.us/manga/The-God-Of-High-School/0{chapter_num}-00{page_number}.png"
+                    )
+                f.write(image.content)
+                print(f"Downloading {manganame}_{chapter_num}_{page_name}.png....")
+            elif page_number in range(10, 100):
+                image = re.get(
+                    f"https://official-other.orience.us/manga/The-God-Of-High-School/0{chapter_num}-0{page_number}.png"
+                    )
+                f.write(image.content)
+                print(f"Downloading {manganame}_{chapter_num}_{page_name}.png....")
+            elif page_number in range(100, lim + 1):
+                image = re.get(
+                    f"https://official-other.orience.us/manga/The-God-Of-High-School/0{chapter_num}-{page_number}.png"
+                    )
+                f.write(image.content)
+                print(f"Downloading {manganame}_{chapter_num}_{page_name}.png....")
 
-def manga_download(manga_name, filename, link):
-    # try:
-    #     os.mkdir(os.path.join(os.getcwd(), folder_name))
-    # except:
-    #     print("error while creating folder for " + folder_name)
-    # os.chdir(os.path.join(os.getcwd(), folder_name))
-    with open(filename, "wb") as f:
-        image = requests.get(link)
-        f.write(image.content)
-        print("downloading... " + filename)
-manga_download(manga_name, page_file_name1, page1)
+
+manga_downloader_edited()
+# this project for example scrapes the popular korean based comic "god of high school" and saves it to your device
